a37039cfa0e8ff8a5ec937bb4446fe1241f0d194
Arthur Alglave <aalglave@microsoft.com>
2022-03-13 19:24:02 +0000

    Add header, styles

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,32 @@
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/projects">Projects</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+
+	ul {
+		display: flex;
+		list-style-type: none;
+		gap: 1rem;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Header from '$lib/components/header.svelte';
+	import '$lib/styles.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>Footer here</footer>
