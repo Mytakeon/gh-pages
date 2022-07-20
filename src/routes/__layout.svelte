ac9dafc3259471b2c29522efd30e7ad30560b87c
Arthur Alglave <aalglave@microsoft.com>
2022-07-20 20:42:01 +0100

    Revert "Test vercel analytics"
    This reverts commit c7fb146aa8defcc709efdabc7b5b211d4be5174b.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,19 +2,11 @@
 	import Header from '$lib/components/header.svelte';
 	import '$lib/app.css';
 	import '$lib/prism.css';
-	import { webVitals } from '$lib/vitals';
-	import { browser } from '$app/env';
-	import { page } from '$app/stores';
-
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
 </script>
+
+<svelte:head>
+	<script data-goatcounter="https://mytakeon.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
+</svelte:head>
 
 <Header />
 
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit()],
-    define: {
-        'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
-    }
+    plugins: [sveltekit()]
 };
 
 export default config;
