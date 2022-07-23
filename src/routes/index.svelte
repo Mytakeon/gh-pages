42c5576ef0c83ee61216064004bc45106ff5e5d7
Arthur Alglave <aalglave@microsoft.com>
2022-07-23 17:15:09 +0100

    Use SVG for cross icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -84,7 +84,19 @@
 			</a>
 		{/each}
 		{#if currentTagName}
-			<a href={`/`} class="cursor-pointer no-underline pl-2">X</a>
+			<a aria-label="View all tags" href={`/`} class="cursor-pointer no-underline pl-2">
+				<svg
+					viewBox="0 0 24 24"
+					width="24"
+					height="24"
+					stroke="currentColor"
+					stroke-width="1.5"
+					fill="none"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" /></svg
+				>
+			</a>
 		{/if}
 	</div>
 </div>
