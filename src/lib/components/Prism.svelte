d3fee87f8dd333220c4dda6d575e27e87fb2629e
Arthur Alglave <aalglave@microsoft.com>
2022-09-03 17:16:53 +0100

    Improve Prism component
    Add line count
    Add copy to clipboard button

--- src/lib/components/Prism.svelte
+++ src/lib/components/Prism.svelte
@@ -12,10 +12,12 @@
 	let language = link.split('.').pop();
 	let source: string;
 	let formattedCode: string | null;
+	let lineCount = 0;
 
 	onMount(async () => {
 		const response = await fetch(link);
 		source = await response.text();
+		lineCount = source.split('\n').length;
 	});
 
 	$: source && highlightCode();
@@ -32,6 +34,10 @@
 		}
 	}
 
+	function copyToClipBoard(value: string) {
+		navigator.clipboard.writeText(value);
+	}
+
 	function getBlobLink(rawLink: string) {
 		// e.g. from: https://raw.githubusercontent.com/Mytakeon/vite-svelte-tailwind/main/src/main.ts
 		// to: https://github.com/Mytakeon/vite-svelte-tailwind/blob/main/src/main.ts
@@ -43,20 +49,41 @@
 </script>
 
 <div>
-	{#if formattedCode}
-		<pre class="language-{language} rounded-t-md gist" command-line data-output="2-17"><code
-				class="language-{language}"
-				>{#if language === 'none'}{formattedCode}{:else}{@html formattedCode}{/if}</code
-			></pre>
-	{:else}
-		<pre command-line data-output="2-17"><code class="language-plain">Loading snippet...</code></pre>
-	{/if}
-	<div class="bg-slate-200 dark:bg-slate-600 rounded-b-md py-1 text-right">
-		<a
-			href={getBlobLink(link)}
-			target="_blank"
-			class="pr-2 no-underline hover:underline"
-			title="View in Github"><code>{fileName}</code></a
+	<div
+		class="bg-slate-100 border border-gray-300 dark:border-gray-500 dark:bg-slate-600 p-2 flex justify-between rounded-t-md align-middle"
+	>
+		<div>
+			<a href={getBlobLink(link)} target="_blank" class="no-underline hover:underline" title="View in Github"
+				><code>{fileName}</code></a
+			>
+			<span class="text-slate-400 mr-1 ml-1">|</span>
+			<code>{lineCount} lines</code>
+		</div>
+		<button title="copy to clipboard" on:click={() => copyToClipBoard(source)}
+			><svg
+				viewBox="0 0 24 24"
+				width="24"
+				height="24"
+				stroke="currentColor"
+				stroke-width="2"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="ml-2 stroke-slate-600 dark:stroke-slate-200 hover:stroke-blue-500 dark:hover:stroke-blue-500"
+				><rect x="9" y="9" width="13" height="13" rx="2" ry="2" /><path
+					d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"
+				/></svg
+			></button
 		>
+	</div>
+	<div>
+		{#if formattedCode}
+			<pre class="language-{language} rounded-b-md gist" command-line data-output="2-17"><code
+					class="language-{language}"
+					>{#if language === 'none'}{formattedCode}{:else}{@html formattedCode}{/if}</code
+				></pre>
+		{:else}
+			<pre command-line data-output="2-17"><code class="language-plain">Loading snippet...</code></pre>
+		{/if}
 	</div>
 </div>
